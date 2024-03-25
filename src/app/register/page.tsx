486082c0c0a5a76bb6114db218556ce032f2dfcd
Luis <lgmarchioreh@gmail.com>
2024-03-24 21:47:09 -0300

    REMOVED(register): Removed unused registration page

--- src/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { RegisterPage } from "../_components/form/RegisterForm";
-
-export default function Register() {
-    return(
-        <main className="h-[100vh]">
-            <RegisterPage/>
-        </main>
-    )
-}
